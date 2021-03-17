8935aa70a77c34696fc539fcb149c4caca04f646
zacksleo <zacksleo@gmail.com>
2021-03-17 11:02:32 +0800

    feat(mybatis): 增加 ExampleMapper

--- mybatis-spring-boot-starter/src/main/java/org/shield/mybatis/mapper/Mapper.java
+++ mybatis-spring-boot-starter/src/main/java/org/shield/mybatis/mapper/Mapper.java
@@ -3,6 +3,7 @@ package org.shield.mybatis.mapper;
 
 import tk.mybatis.mapper.common.BaseMapper;
 import tk.mybatis.mapper.common.ConditionMapper;
+import tk.mybatis.mapper.common.ExampleMapper;
 import tk.mybatis.mapper.common.IdsMapper;
 import tk.mybatis.mapper.common.special.InsertListMapper;
 
@@ -13,6 +14,6 @@ import tk.mybatis.mapper.common.special.InsertListMapper;
  * @author zacksleo@gmail.com
  */
 public interface Mapper<T>
-        extends BaseMapper<T>, ConditionMapper<T>, IdsMapper<T>, InsertListMapper<T> {
+        extends BaseMapper<T>, ConditionMapper<T>, IdsMapper<T>, InsertListMapper<T>, ExampleMapper<T> {
 
 }
